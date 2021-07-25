1fe9491171f1f50726253a86bb4fa777aeb657cb
only3seconds <TripleP3@163.com>
2021-07-25 23:02:40 +0800

    Leetcode206 反转链表优化

--- leetcode-linked-list/src/main/java/org/thinking/linked/list/easy/Leetcode206.java
+++ leetcode-linked-list/src/main/java/org/thinking/linked/list/easy/Leetcode206.java
@@ -11,27 +11,48 @@ public class Leetcode206 {
    * 头插法
    * 1. 注意"节点引用"和"节点"的区别
    * 2. 注意链表的尾节点要置空
-   * 3. 遍历链表时要注意结束条件，要考虑最后一个节点是否也处理了
+   * 3. 通过新建虚拟头节点统一操作
    */
   public ListNode reverseList(ListNode head) {
-    if (head == null || head.next == null) {
+    if (head == null) {
       return head;
     }
     ListNode newHead = head;
     ListNode p = head.next;
-    ListNode post = p.next;
+    ListNode post; // 不要在这里急着给post赋值
 
     newHead.next = null;
-    while (post != null) {
+    while (p != null) {
+      post = p.next; // 先保存下一个节点
       p.next = newHead;
       newHead = p;
       p = post;
-      post = p.next;
     }
-    p.next = newHead;
-    newHead = p;
 
     return newHead;
+  }
+
+  /**
+   * 增加虚拟头节点
+   * @param head
+   * @return
+   */
+  public ListNode reverseList1(ListNode head) {
+    if (head == null) {
+      return head;
+    }
+    ListNode visualHead = new ListNode();
+    visualHead.next = null;
+    ListNode p = head;
+    ListNode post;
+    while (p != null) {
+      post = p.next;
+      p.next = visualHead.next;
+      visualHead.next = p;
+      p = post;
+    }
+
+    return visualHead.next;
   }
 
   public class ListNode {
