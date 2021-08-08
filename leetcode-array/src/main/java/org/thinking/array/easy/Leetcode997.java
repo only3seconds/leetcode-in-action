75db1941346c0f5c23e768a5a5427049b25645f2
only3seconds <TripleP3@163.com>
2021-08-08 23:00:28 +0800

    Leetcode997 找到小镇的法官，投票思想

--- leetcode-array/src/main/java/org/thinking/array/easy/Leetcode997.java
+++ leetcode-array/src/main/java/org/thinking/array/easy/Leetcode997.java
@@ -6,10 +6,14 @@ import java.util.Map;
 
 /**
  * 找到小镇的法官
+ *
  * @author thinking-ppp 2021/8/8
  */
 public class Leetcode997 {
 
+  /**
+   * 方法一：使用HashMap
+   */
   public int findJudge(int n, int[][] trust) {
     if (trust == null || trust.length == 0) {
       if (n == 1) {
@@ -38,14 +42,39 @@ public class Leetcode997 {
       }
     }
 
-
-    for (HashSet<Integer> set: map.values()) {
+    for (HashSet<Integer> set : map.values()) {
       if (!set.contains(maybeJudge)) {
         return -1;
       }
     }
 
     return maybeJudge;
+  }
+
+  /**
+   * 方法二：投票思想，使用两个一纬数组
+   * @param n
+   * @param trust
+   * @return
+   */
+  public int findJudge2(int n, int[][] trust) {
+    // 投出去的票数
+    int[] vote1 = new int[n + 1];
+    // 收获票数
+    int[] vote2 = new int[n + 1];
+
+    for (int i = 0; i < trust.length; i++) {
+      vote1[trust[i][0]]++;
+      vote2[trust[i][1]]++;
+    }
+
+    for (int i = 1; i <= n; i++) {
+      if (vote1[i] == 0 && vote2[i] == n - 1) {
+        return i;
+      }
+    }
+
+    return -1;
   }
 
 }
